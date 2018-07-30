bbb8ef30c179bc9e83166b71c927757a36953370
XIECI <cisnoc.xie@oocl.com>
2018-07-30 20:01:00 +0800

    pass e3, e6, e5 checkThirdSpeedRecordWasSetIn1955

--- src/test/java/exercises/RestAssuredExercises5Test.java
+++ src/test/java/exercises/RestAssuredExercises5Test.java
@@ -6,6 +6,8 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.core.Is.is;
 
 public class RestAssuredExercises5Test {
 
@@ -33,7 +35,9 @@ public class RestAssuredExercises5Test {
 		given().
 			spec(requestSpec).
 		when().
-		then();
+				get("/xml/speedrecords").
+		then()
+		.body("speedRecords.car[2].year", equalTo("1955"));
 	}
 	
 	/*******************************************************
